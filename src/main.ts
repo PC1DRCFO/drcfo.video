08d2a0bbd6b857bc56a79e4badf0dbde54877c73
Aniket <pc1.drcfo@gmail.com>
2022-02-28 17:39:46 +0530

    template

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 /*!
 
 =========================================================
-* Argon Dashboard Angular - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-angular
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-angular/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
 
 =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+=========================================================
+
+*
 
 */
 import { enableProdMode } from '@angular/core';
@@ -22,8 +16,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    .catch(err => console.error(err));
